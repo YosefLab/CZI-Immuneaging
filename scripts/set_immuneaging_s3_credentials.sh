ce83d6ea8d19e4ec0515b10028d61364fe8eb53c
galen xing <gx2113@columbia.edu>
2021-03-25 01:25:15 -0700

    documentation edits

--- scripts/set_immuneaging_s3_credentials.sh
+++ scripts/set_immuneaging_s3_credentials.sh
@@ -4,12 +4,12 @@
 # DO NOT DISTRIBUTE #
 #####################
 
-# Access script for First Name, Last Name
+# Sets AWS credentials for First Name, Last Name
 
 # usage: bash set_immuneaging_s3_credentials.sh
 
 # This script will set your personal access keys for the Yosef Lab's immuneaging s3 bucket.
-# This script should be run once per bash session and will only allow read access in said
+# This script should be run once per bash session and will only allow access in said
 # bash session. Once the session is closed, access will cease and the aws credentials will 
 # revert back to the aws config credentials.
 
