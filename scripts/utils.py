8c724837f6b28333526e07d963a96f1cb5a7f361
Elior Rahmani <erahmani@Eliors-MacBook-Pro.local>
2021-03-31 16:03:26 -0700

    utils.py

---
+++ scripts/utils.py
@@ -0,0 +1,34 @@
+import json
+
+def load_configs(filename):
+    with open(filename) as f: 
+        data = f.read()	
+    configs = json.loads(data)
+    validate_configs(configs)
+    return configs
+
+def validate_configs(configs):
+	assert(configs["aligner"] == "cellranger")
+
+def set_access_keys(filepath):
+	"""
+	Sets the user's access keys to the AWS S3 bucket.
+
+	Assumes this file includes only two uncommented lines with keys:
+	export AWS_ACCESS_KEY_ID=<key>
+	export AWS_SECRET_ACCESS_KEY=<key>
+	"""
+	keys = {}
+	with open(filepath) as fp:
+		for i in fp:
+			if len(i.rstrip()) and i[0]!="#":
+				cmd = i.rstrip().split(" ")
+				assert(cmd[0] == "export")
+				cmd.pop(0)
+				for i in range(len(cmd)-1,-1,-1):
+					if len(cmd[i]) == 0:
+						cmd.pop(i)
+				pair = "".join(cmd).split("=")
+				keys[pair[0]] = pair[1]
+	for k in keys:
+		os.environ[k] = keys[k]
