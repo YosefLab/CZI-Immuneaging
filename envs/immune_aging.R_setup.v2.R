e26160359d4c762230d08e45425092ac1a19bed3
Elior Rahmani <erahmani@Eliors-MacBook-Pro.local>
2021-08-05 16:22:10 -0700

    updated env v2

--- envs/immune_aging.R_setup.v2.R
+++ envs/immune_aging.R_setup.v2.R
@@ -1,7 +1,7 @@
 # This script should be executed in an R session after creating a conda environment
 library("devtools")
 download.file("https://github.com/campbio/celda/archive/refs/tags/v1.7.3.zip", destfile = "v1.7.3.zip")
-devtools::install_local("v1.7.3.zip") # when prompted to update packages - update all
+devtools::install_local("v1.7.3.zip") # when prompted to update packages - update none
 # for vision (including anndata for converting h5ad files to R objects):
 install_version("anndata", version = "0.7.5.2", repos = "http://cran.us.r-project.org")
 library("anndata")
