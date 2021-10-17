eaa55c2b4a4d34ccb1cdf51adc337a837793e45c
Valeh Valiollah Pour Amiri <4193454+watiss@users.noreply.github.com>
2021-10-16 19:28:09 -0700

    set up aws creds

--- data_processing/scripts/digest_process_sample_logs.py
+++ data_processing/scripts/digest_process_sample_logs.py
@@ -1,11 +1,11 @@
 ## This script can be used to digest a set of logs from the proess_sample.py script for a given donor,seq_run pair.
-## Run as follows: python digest_process_sample_logs.py <donor_id> <seq_run> <logs_location> <version> <working_dir>
+## Run as follows: python digest_process_sample_logs.py <donor_id> <seq_run> <logs_location> <version> <working_dir> <s3_access_file>
 
 import sys
 import os
 import math
 import utils
-import logger
+from logger import RichLogger, is_alertable_log_line
 
 def get_process_sample_log_file_name(sample_id: str, library_type: str, version: str):
     prefix = "{}_{}".format(sample_id, library_type)
@@ -13,13 +13,19 @@ def get_process_sample_log_file_name(sample_id: str, library_type: str, version:
 
 donor_id = sys.argv[1]
 seq_run = sys.argv[2]
-logs_location = sys.argv[3] # must be one of "aws" or path to the logs location on the local disk
+logs_location = sys.argv[3] # must be either "aws" or the absolute path to the logs location on the local disk
 version = sys.argv[4] # must be either the version to use (e.g. "v1") or the latest version for each sample ("latest")
-working_dir = sys.argv[5] # must be either "" or the path to the local directory where we will temporarily place the logs downloaded from aws - only used if logs_location is "aws"
+working_dir = sys.argv[5] # must be either "" or the absolute path to the local directory where we will temporarily place the logs downloaded from aws - only used if logs_location is "aws"
+s3_access_file = sys.argv[6] # must be either "" or the absolute path to the aws credentials file - only used if logs_location is "aws"
 
 # sanity check command input
 assert(logs_location=="aws" or os.path.isdir(logs_location))
 assert(working_dir == "" if logs_location!="aws" else os.path.isdir(working_dir))
+assert(s3_access_file == "" if logs_location!="aws" else os.path.isfile(s3_access_file))
+
+# set aws credentials
+if logs_location == "aws":
+    utils.set_access_keys(s3_access_file)
 
 # gather the full set of sample id's we are interested in
 samples = utils.read_immune_aging_sheet("Samples", quiet=True)
@@ -29,6 +35,7 @@ sample_ids = samples[indices]["Sample_ID"]
 library_type = "GEX" # we assume this for now, if this changes we can have it be passed as a command line param
 
 # if the logs location is aws, download all log files to the local working directory
+logger = RichLogger()
 if logs_location == "aws":
     for sample_id in sample_ids:
         prefix = "{}_{}".format(sample_id, library_type)
@@ -55,17 +62,16 @@ for sample_id in sample_ids:
     for line in lines:
         # for now, we only print logs above a hard coded level threshold (ERROR),
         # but we can extend this script to take a user-provided threshold 
-        if logger.is_alertable_log_line(line):
+        if is_alertable_log_line(line):
             if filepath not in log_lines_to_print:
                 log_lines_to_print[filepath] = [line]
             else:
                 log_lines_to_print[filepath].append(line)
 
-rich_logger = logger.RichLogger()
 if len(log_lines_to_print) == 0:
-    rich_logger.add_to_log("No relevant log lines were found.", "info")
+    logger.add_to_log("No relevant log lines were found.", "info")
 else:
-    rich_logger.add_to_log("Found the following relevant log lines.", "info")
+    logger.add_to_log("Found the following relevant log lines.", "info")
     first_item = True
     for key,value in log_lines_to_print.items():
         if not first_item:
