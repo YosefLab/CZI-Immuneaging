ece63f5f26954f78e763ed940bc7a15fc11f8c38
Valeh Valiollah Pour Amiri <4193454+watiss@users.noreply.github.com>
2021-10-20 11:28:04 -0700

    Catch and print exceptions raised during log digestion and skip log files that don't exist (#19)

--- data_processing/scripts/digest_process_sample_logs.py
+++ data_processing/scripts/digest_process_sample_logs.py
@@ -4,6 +4,8 @@
 import sys
 import os
 import math
+import traceback
+
 import utils
 from logger import RichLogger, is_alertable_log_line
 
@@ -11,11 +13,15 @@ def get_process_sample_log_file_name(sample_id: str, library_type: str, version:
     prefix = "{}_{}".format(sample_id, library_type)
     return "process_sample.{}.{}.log".format(prefix, version)
 
+def remove_logs(logs_dir: str):
+    if os.path.isdir(logs_dir):
+        os.system("rm {}/*".format(logs_dir))
+
 donor_id = sys.argv[1]
 seq_run = sys.argv[2]
 logs_location = sys.argv[3] # must be either "aws" or the absolute path to the logs location on the local disk
 version = sys.argv[4] # must be either the version to use (e.g. "v1") or the latest version for each sample ("latest")
-working_dir = sys.argv[5] # must be either "" or the absolute path to the local directory where we will temporarily place the logs downloaded from aws - only used if logs_location is "aws"
+working_dir = sys.argv[5] # must be either "" or the absolute path to the local directory where we will place the logs downloaded from aws - only used if logs_location is "aws"
 s3_access_file = sys.argv[6] # must be either "" or the absolute path to the aws credentials file - only used if logs_location is "aws"
 
 # sanity check command input
@@ -33,59 +39,68 @@ indices = samples["Donor ID"] == donor_id
 sample_ids = samples[indices]["Sample_ID"]
 
 library_type = "GEX" # we assume this for now, if this changes we can have it be passed as a command line param
+downloaded_from_aws = logs_location == "aws"
 
-# if the logs location is aws, download all log files to the local working directory
-logger = RichLogger()
-downloaded_from_aws = False
-if logs_location == "aws":
+try:
+    # if the logs location is aws, download all log files to the local working directory
+    logger = RichLogger()
+    if logs_location == "aws":
+        logs_location = working_dir
+        for sample_id in sample_ids:
+            prefix = "{}_{}".format(sample_id, library_type)
+            filename = get_process_sample_log_file_name(sample_id, library_type, version)
+            sync_cmd = 'aws s3 sync --no-progress s3://immuneaging/processed_samples/{}/{} {} --exclude "*" --include {}'.format(
+                prefix, version, working_dir, filename)
+            logger.add_to_log("syncing {}...".format(filename))
+            logger.add_to_log("sync_cmd: {}".format(sync_cmd))
+            logger.add_to_log("aws response: {}\n".format(os.popen(sync_cmd).read()))
+
+    if version == "latest":
+        # TODO issue #15
+        raise NotImplementedError
+
+    # for each sample id, parse its process_sample logs and report any noteworthy log events
+    log_lines_to_print = {}
     for sample_id in sample_ids:
-        prefix = "{}_{}".format(sample_id, library_type)
         filename = get_process_sample_log_file_name(sample_id, library_type, version)
-        sync_cmd = 'aws s3 sync --no-progress s3://immuneaging/processed_samples/{}/{} {} --exclude "*" --include {}'.format(
-            prefix, version, working_dir, filename)
-        logger.add_to_log("syncing {}...".format(filename))
-        logger.add_to_log("sync_cmd: {}".format(sync_cmd))
-        logger.add_to_log("aws response: {}\n".format(os.popen(sync_cmd).read()))
-    logs_location = working_dir
-    downloaded_from_aws = True
+        filepath = os.path.join(logs_location, filename)
+        if not os.path.isfile(filepath):
+            logger.add_to_log("File not found: {}. Skipping.".format(filepath))
+            continue
+        lines = []
+        with open(filepath, 'r') as f:
+            lines = f.readlines()
+        for line in lines:
+            # for now, we only print logs above a hard coded severity threshold (see `is_alertable_log_line`),
+            # but we can extend this script to take a user-provided threshold 
+            if is_alertable_log_line(line):
+                if filepath not in log_lines_to_print:
+                    log_lines_to_print[filepath] = [line]
+                else:
+                    log_lines_to_print[filepath].append(line)
 
-if version == "latest":
-    # TODO issue #15
-    raise NotImplementedError
+    if len(log_lines_to_print) == 0:
+        logger.add_to_log("No relevant log lines were found.", "info")
+    else:
+        logger.add_to_log("Found the following relevant log lines.", "info")
+        first_item = True
+        for key,value in log_lines_to_print.items():
+            if not first_item:
+                # draw a separator line between files
+                width = os.get_terminal_size().columns / 5
+                print(" " * math.floor(width)  + "\u2014" * 3 * math.floor(width) + " " * math.floor(width) + "\n")
+            file_name = key.split("/")[-1]
+            print(file_name + ":\n")
+            for line in value:
+                print("\t" + line)
+            first_item = False
 
-# for each sample id, parse its process_sample logs and report any noteworthy log events
-log_lines_to_print = {}
-for sample_id in sample_ids:
-    filename = get_process_sample_log_file_name(sample_id, library_type, version)
-    filepath = os.path.join(logs_location, filename)
-    lines = []
-    with open(filepath, 'r') as f:
-        lines = f.readlines()
-    for line in lines:
-        # for now, we only print logs above a hard coded severity threshold (see `is_alertable_log_line`),
-        # but we can extend this script to take a user-provided threshold 
-        if is_alertable_log_line(line):
-            if filepath not in log_lines_to_print:
-                log_lines_to_print[filepath] = [line]
-            else:
-                log_lines_to_print[filepath].append(line)
-
-if len(log_lines_to_print) == 0:
-    logger.add_to_log("No relevant log lines were found.", "info")
-else:
-    logger.add_to_log("Found the following relevant log lines.", "info")
-    first_item = True
-    for key,value in log_lines_to_print.items():
-        if not first_item:
-            # draw a separator line between files
-            width = os.get_terminal_size().columns / 5
-            print(" " * math.floor(width)  + "\u2014" * 3 * math.floor(width) + " " * math.floor(width) + "\n")
-        file_name = key.split("/")[-1]
-        print(file_name + ":\n")
-        for line in value:
-            print("\t" + line)
-        first_item = False
-
-# clean up the logs we downloaded from aws if any
-if downloaded_from_aws:
-    os.system("rm {}/*".format(logs_location))
+    # clean up the logs we downloaded from aws if any
+    if downloaded_from_aws:
+        remove_logs(logs_location)
+except Exception as err:
+    logger.add_to_log("Execution failed with the following error:\n{}".format(traceback.format_exc()), "critical")
+    # clean up the logs we downloaded from aws if any
+    if downloaded_from_aws:
+        remove_logs(logs_location)
+    sys.exit()
