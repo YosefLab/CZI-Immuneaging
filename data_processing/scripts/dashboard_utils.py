80f4ec27f32cf0a7d666cfeb6c0fd4e505fa39a4
Valeh Valiollah Pour Amiri <4193454+watiss@users.noreply.github.com>
2021-11-11 19:20:58 -0800

    fix a bugnch of things

--- data_processing/scripts/dashboard_utils.py
+++ data_processing/scripts/dashboard_utils.py
@@ -14,7 +14,8 @@ logger = RichLogger()
 
 def get_tissue_coverage_csv():
     logger.add_to_log("Downloading the Donors sheet from Google drive...")
-    donors = utils.read_immune_aging_sheet("Donors")
+    donors_csv = utils.read_immune_aging_sheet("Donors")
+    donors = donors_csv["Donor ID"]
     logger.add_to_log("Downloading the Samples sheet from Google drive...")
     samples = utils.read_immune_aging_sheet("Samples")
     tissues = np.unique(samples["Organ"])
@@ -28,21 +29,24 @@ def get_tissue_coverage_csv():
         # build the dictionary of tissue to a list of tissue stim status
         # a tissue can have more than one stim status (e.g. it can be nonstim and stim)
         tissue_stimulation = {tissue: [] for tissue in tissues}
-        for i in range(len(organs)):
-            stim_status = tissue_stimulation[organs[i]]
+        for idx,_ in organs.items():
+            stim_status = tissue_stimulation[organs[idx]]
             # if for this donor, this tissue was so far found to have no data then set it to
             # stimulations[i]. If it was already reported to be stimulated with stimulations[i]
             # then no need to do anything. Otherwise, append stimulations[i] to whatever we have
             # so far
-            if stim_status == [] or stimulations[i] not in stim_status:
-                stim_status.append(stimulations[i])
-            tissue_stimulation[organs[i]] = stim_status
+            if stim_status == [] or stimulations[idx] not in stim_status:
+                stim_status.append(stimulations[idx])
+            tissue_stimulation[organs[idx]] = stim_status
+
+        print(tissue_stimulation)
 
         # fill in the row and add it to the list of rows
         csv_row = {}
         for tissue in tissues:
+            tissue_stimulation[tissue].sort()
             stim_status = tissue_stimulation[tissue]
-            csv_row[tissue] = "No Data" if stim_status == [] else stim_status.sort().join(" ➕ ")
+            csv_row[tissue] = "No Data" if stim_status == [] else " ➕  ")join(stim_status)
         csv_rows.append(csv_row)
 
     # write the csv
