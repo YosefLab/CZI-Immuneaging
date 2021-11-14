1ed6fd6965bb0d8820eb66299e769f5d371a369c
Elior Rahmani <elior.rahmani@gmail.com>
2021-11-14 09:22:14 -0800

    Generate tissue figures (#35)
    * generating figures for integrated tissues
    
    * added figure generation for integrated tissues and urls for the dashboard; added script for generating config files for tissue integration
    
    * uncommented the deletion of working dir at the end
    
    * added donors age information for the tissue integration table
    
    * added figures for age and sex
    
    * remove the entire working dir
    
    * fixed rm of working dir
    
    Co-authored-by: Elior Rahmani <erahmani@Eliors-MacBook-Pro.local>

--- data_processing/scripts/dashboard_utils.py
+++ data_processing/scripts/dashboard_utils.py
@@ -8,9 +8,12 @@ import sys
 import io
 import csv
 import numpy as np
+import pandas as pd
 import os
 import traceback
 import scanpy as sc
+import zipfile
+import shutil
 
 import utils
 from logger import RichLogger
@@ -61,6 +64,80 @@ def get_tissue_coverage_csv():
     print(csv_file.getvalue())
     csv_file.close()
 
+def generate_tissue_integration_figures(adata, tissue, version, working_dir, base_aws_url, base_s3_url, base_s3_dir):
+    
+    # add two age fields for the umaps - age as a quantitative variable and as a categorical variable using 3 age groups
+    ages_str = np.unique(adata.obs["age"])
+    ages = {}
+    for a in ages_str:
+        try:
+            ages[a] = float(a)
+        except ValueError:
+            # this is a range and not a number
+            b = sorted([float(j) for j in a.split("-")])
+            ages[a] = b[0]+(b[1]-b[0])/2 # take the middle of the range
+    adata.obs["age_continuous"] = np.array([ages[j] for j in adata.obs["age"].values])
+    age_categorical = []
+    grp1_label = "{}-39 y/o".format(round(np.min(adata.obs["age_continuous"].values)))
+    grp2_label = "40-59 y/o"
+    grp3_label = "60-{} y/o".format(round(np.max(adata.obs["age_continuous"].values)))
+    for j in adata.obs["age_continuous"].values:
+        if j <= 40:
+            age_categorical.append(grp1_label)
+        elif j <= 60:
+            age_categorical.append(grp2_label)
+        else:
+            age_categorical.append(grp3_label)
+    adata.obs["age_categorical"] = pd.Categorical(np.array(age_categorical))
+
+    fields_to_plot = ["site", "donor_id", "sample_id", "total_counts", "sex", "age_continuous", "age_categorical"]
+
+    logger.add_to_log("Generating figures for tissue: {}, version: {}\n".format(tissue,version))
+    figures_dir = os.path.join(working_dir,"figures")
+    os.system("mkdir -p " + figures_dir)
+    prefix = "{}.{}".format(tissue, version)
+    
+    # use umap embeddings based on totalVI if available; otherwise based on scvi
+    if "X_umap_totalvi_integrated" in adata.obsm:
+        umap_key = "X_umap_totalvi_integrated"
+    else:
+        umap_key = "X_umap_scvi_integrated"
+    adata.obsm["X_umap"] = adata.obsm[umap_key]
+    
+    for field in fields_to_plot:
+        # looks like sc.pl.umap cannot take an absolute path; move files from the output dir of sc.pl.umap to the working directory
+        filename = ".{}.{}.pdf".format(prefix,field)
+        sc.pl.umap(adata, color=[field], save = filename)
+        filename = "umap" + filename
+        os.rename("figures/" + filename, os.path.join(figures_dir, filename))
+
+    # for each celltypist model used plot a umap colored by the predicted cell types
+    mdl = 1
+    while "celltypist_model.{}".format(str(mdl)) in adata.obs:
+        mdl_name = "celltypist_{}".format(adata.obs["celltypist_model." + str(mdl)][1].split("/")[-1].split(".")[0])
+        filename = ".{}.{}.pdf".format(prefix,mdl_name)
+        sc.pl.umap(adata, color=["celltypist_predicted_labels.{}".format(str(mdl))], save = filename)
+        filename = "umap" + filename
+        os.rename("figures/" + filename, os.path.join(figures_dir, filename))
+        mdl += 1
+
+    logger.add_to_log("Saving .zip file with the figures...")
+    zip_filename = "{}.figures.zip".format(prefix)
+    figures_file_path = os.path.join(working_dir, zip_filename)
+    zipf = zipfile.ZipFile(figures_file_path, 'w', zipfile.ZIP_DEFLATED)
+    utils.zipdir(figures_dir, zipf)
+    zipf.close()
+    
+    logger.add_to_log("Uploading {} to aws...".format(zip_filename))
+    aws_destination = "{}/{}/{}/{}".format(base_s3_url, base_s3_dir, tissue, version)
+    sync_cmd = 'aws s3 sync --no-progress {} {} --exclude "*" --include {}'.format(working_dir, aws_destination, zip_filename)
+    logger.add_to_log("sync_cmd: {}".format(sync_cmd))
+    logger.add_to_log("aws response: {}\n".format(os.popen(sync_cmd).read()))
+    
+    shutil.rmtree("figures")
+    figures_url = "{}{}/{}/{}/{}".format(base_aws_url, base_s3_dir, tissue, version, zip_filename)
+    return figures_url
+
 def get_tissue_integration_results_csv(working_dir: str, s3_access_file: str):
     logger.add_to_log("Downloading the Samples sheet from Google drive...")
     samples = utils.read_immune_aging_sheet("Samples")
@@ -70,14 +147,17 @@ def get_tissue_integration_results_csv(working_dir: str, s3_access_file: str):
     CSV_HEADER_TISSUE: str = "Tissue"
     CSV_HEADER_DONOR_COUNT: str = "# Donors"
     CSV_HEADER_DONORS: str = "Donors"
+    CSV_HEADER_DONORS_AGE: str = "Donors age"
     CSV_HEADER_CELL_COUNT: str = "# Cells"
     CSV_HEADER_SAMPLE_COUNT: str = "# Samples"
     CSV_HEADER_SAMPLES: str = "Samples"
     CSV_HEADER_ANNDATA: str = "Anndata URL"
+    CSV_HEADER_FIGURES: str = "Figures URL"
 
     # prepare the needful for downloading data from aws
     utils.set_access_keys(s3_access_file)
-    BASE_AWS_URL = "https://immuneaging.s3.us-west-1.amazonaws.com"
+    #BASE_AWS_URL = "https://immuneaging.s3.us-west-1.amazonaws.com"
+    BASE_AWS_URL = "https://s3.console.aws.amazon.com/s3/object/immuneaging?prefix="
     BASE_S3_URL = "s3://immuneaging"
     BASE_S3_DIR = "integrated_samples/tissue_level"
     version = "v1" # TODO add ability to get the latest version (github issue #32)
@@ -89,22 +169,24 @@ def get_tissue_integration_results_csv(working_dir: str, s3_access_file: str):
         csv_row[CSV_HEADER_TISSUE] = tissue
         csv_row[CSV_HEADER_DONOR_COUNT] = -1
         csv_row[CSV_HEADER_DONORS] = []
+        csv_row[CSV_HEADER_DONORS_AGE] = []
         csv_row[CSV_HEADER_CELL_COUNT] = -1
         csv_row[CSV_HEADER_SAMPLE_COUNT] = -1
         csv_row[CSV_HEADER_SAMPLES] = []
         csv_row[CSV_HEADER_ANNDATA] = "Pending processing"
+        csv_row[CSV_HEADER_FIGURES] = "Pending processing"
 
         # download the integrated anndata from aws
         try:
             file_name = "{}.{}.h5ad".format(tissue, version)
 
             # first check to see if we have an anndata for this tissue
-            ls_cmd = "aws s3 ls {}/{}/{}/{}".format(BASE_S3_URL, BASE_S3_DIR, tissue, version)
+            ls_cmd = "aws s3 ls {}/{}/{}/{}/".format(BASE_S3_URL, BASE_S3_DIR, tissue, version)
             files = os.popen(ls_cmd).read()
             logger.add_to_log("aws response: {}\n".format(files))
             found = False
             for f in files.rstrip().split('\n'):
-                if f.split('/')[-1] == file_name:
+                if f.split(' ')[-1] == file_name:
                     found = True
                     break
 
@@ -112,7 +194,7 @@ def get_tissue_integration_results_csv(working_dir: str, s3_access_file: str):
             if not found:
                 logger.add_to_log("Integrated annotated data not found for issue {}".format(tissue))
             else:
-                csv_row[CSV_HEADER_ANNDATA] = "{}/{}/{}/{}/{}".format(BASE_AWS_URL, BASE_S3_DIR, tissue, version, file_name)
+                csv_row[CSV_HEADER_ANNDATA] = "{}{}/{}/{}/{}".format(BASE_AWS_URL, BASE_S3_DIR, tissue, version, file_name)
                 dir_name = "{}/{}/{}/{}/".format(BASE_S3_URL, BASE_S3_DIR, tissue, version)
                 sync_cmd = 'aws s3 sync --no-progress {} {} --exclude "*" --include {}'.format(dir_name, working_dir, file_name)
                 logger.add_to_log("sync_cmd: {}".format(sync_cmd))
@@ -124,13 +206,22 @@ def get_tissue_integration_results_csv(working_dir: str, s3_access_file: str):
                 csv_row[CSV_HEADER_CELL_COUNT] = adata.n_obs
                 csv_row[CSV_HEADER_SAMPLES] = np.unique(adata.obs["sample_id"])
                 csv_row[CSV_HEADER_SAMPLE_COUNT] = len(csv_row[CSV_HEADER_SAMPLES])
-                csv_row[CSV_HEADER_DONORS] = np.unique(adata.obs["donor_id"])
+                donor_ids = np.unique(adata.obs["donor_id"])
+                csv_row[CSV_HEADER_DONORS] = donor_ids
                 csv_row[CSV_HEADER_DONOR_COUNT] = len(csv_row[CSV_HEADER_DONORS])
+                ages = []
+                for donor_id in donor_ids:
+                    ages.append(np.unique(adata[adata.obs["donor_id"] == donor_id,].obs["age"])[0])
+                csv_row[CSV_HEADER_DONORS_AGE] = np.array(ages)
+                
+                # generate figures
+                csv_row[CSV_HEADER_FIGURES] = generate_tissue_integration_figures(adata, tissue, version, working_dir, BASE_AWS_URL, BASE_S3_URL, BASE_S3_DIR)
+
                 # clean up
-                os.system("rm {}/*".format(working_dir))
+                os.system("rm -r {}/*".format(working_dir))
         except Exception:
             logger.add_to_log("Execution failed for tissue {} with the following error:\n{}".format(tissue, traceback.format_exc()), "critical")
-            os.system("rm {}/*".format(working_dir))
+            os.system("rm -r {}/*".format(working_dir))
             logger.add_to_log("Continuing execution for other tissues...")
             continue
         csv_rows.append(csv_row)
@@ -141,10 +232,12 @@ def get_tissue_integration_results_csv(working_dir: str, s3_access_file: str):
         CSV_HEADER_TISSUE,
         CSV_HEADER_DONOR_COUNT,
         CSV_HEADER_DONORS,
+        CSV_HEADER_DONORS_AGE,
         CSV_HEADER_CELL_COUNT,
         CSV_HEADER_SAMPLE_COUNT,
         CSV_HEADER_SAMPLES,
-        CSV_HEADER_ANNDATA,    
+        CSV_HEADER_ANNDATA,
+        CSV_HEADER_FIGURES,
     ]
     writer = csv.DictWriter(csv_file, fieldnames=field_names)
     writer.writeheader()
---
+++ data_processing/scripts/generate_tissue_integration_config_files.py
@@ -0,0 +1,93 @@
+## This script generates configuration files for integrate_samples.py (one configuration file per tissue - for integrating all currently available samples from the given tissue type)
+## run as follows (use absolute paths): python generate_tissue_integration_config_files.py <code_path> <output_destination> <s3_access_file>
+
+import sys
+import os
+import json
+import subprocess
+import numpy as np
+import re 
+
+code_path = sys.argv[1]
+output_destination = sys.argv[2]
+s3_access_file = sys.argv[3]
+
+sys.path.append(code_path)
+from utils import *
+
+python_env = "immune_aging.py_env.v3"
+
+integration_configs = {
+        "sandbox_mode": "False",
+        "data_owner": "erahmani",
+        "code_path": code_path,
+        "output_destination": output_destination,
+        "s3_access_file": s3_access_file,
+        "integration_level": "tissue",
+        "n_highly_variable_genes": 3000,
+        "highly_variable_genes_flavor": "seurat_v3",
+        "scvi_max_epochs": 400,
+        "totalvi_max_epochs": 400,
+        "use_layer_norm": "none",
+        "use_batch_norm": "both",
+        "early_stopping": "True",
+        "early_stopping_patience": 45,
+        "batch_size": 1024,
+        "limit_train_batches": 20,
+        "neighborhood_graph_n_neighbors": 15,
+        "umap_min_dist": 0.5,
+        "umap_spread": 1.0,
+        "umap_n_components": 2,
+        "vdj_genes": "s3://immuneaging/vdj_genes/vdj_gene_list_v1.csv",
+        "python_env_version": python_env,
+        "r_setup_version": "immune_aging.R_setup.v2"
+    }
+
+set_access_keys(s3_access_file)
+
+samples = read_immune_aging_sheet("Samples")
+tissues = np.unique(samples["Organ"][np.logical_not(pd.isnull(samples["Organ"]))])
+
+outfile = open(os.path.join(code_path,"integrate_samples_runs.sh"),'w') 
+outfile.write("source activate {}\n".format(python_env))
+
+for tissue in tissues:
+    # get all samples from the requested tissue as appears in the google spreadsheet
+    final_sample_ids = []
+    versions = []
+    indices = samples["Organ"] == tissue
+    sample_ids = samples["Sample_ID"][indices]
+    for sample_id in sample_ids:
+        ls_cmd = "aws s3 ls s3://immuneaging/processed_samples/{}_GEX --recursive".format(sample_id)
+        ls  = os.popen(ls_cmd).read()
+        if len(ls) == 0:
+            continue
+        # find the latest version available
+        filenames = ls.split("\n")
+        latest_version = -1
+        for filename in filenames:
+            p = re.search("(.)(\d)+(.)log$", filename)
+            if bool(p):
+                version = int(filename[p.span()[0]+1:-4])
+                if latest_version<version:
+                    latest_version = version
+        if latest_version>-1:
+            version = "v"+str(latest_version)
+            # check if h5ad file is available
+            ls_cmd = "aws s3 ls s3://immuneaging/processed_samples/{0}_GEX/{1}/{0}_GEX.processed.{1}.h5ad".format(sample_id,version)
+            if len(os.popen(ls_cmd).read())>0:
+                versions.append(version)
+                final_sample_ids.append(sample_id)
+    if len(final_sample_ids)>1:
+        tissue_integration_configs = integration_configs
+        tissue_integration_configs["output_prefix"] = tissue
+        tissue_integration_configs["sample_ids"] = ",".join(final_sample_ids)
+        tissue_integration_configs["processed_sample_configs_version"] = ",".join([str(i) for i in versions])
+        filename = os.path.join(output_destination,"integrate_samples.{}.configs.txt".format(tissue))
+        with open(filename, 'w') as f:
+            json.dump(tissue_integration_configs, f)
+        print("generated configs file " + filename)
+        outfile.write("python integrate_samples.py {}\n".format(filename))
+
+outfile.write("conda deactivate")
+outfile.close()
