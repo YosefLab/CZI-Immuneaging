ef64bff1c53baf6c65ce1ba008789511acd53d5e
Valeh Valiollah Pour Amiri <4193454+watiss@users.noreply.github.com>
2022-01-12 17:59:07 -0800

    fix bug in build_adata_from_ir_libs

--- data_processing/scripts/process_sample.py
+++ data_processing/scripts/process_sample.py
@@ -234,15 +234,15 @@ def build_adata_from_ir_libs(lib_type: str, library_ids_ir: List[str]) -> AnnDat
     # GEX libs where concatenated. This is important because the concat operation appends a -N suffix to all cells where N is the batch
     # id (N=0..len(lib_ids)-1). If we don't do this, cells from IR libs and GEX libs won't match due to mismatching -N suffix.
     logger.add_to_log("Concatenating all cells of sample {} from available {} libraries...".format(sample_id, lib_type))
-    adata = adata_dict[library_ids_ir[0]]
+    adata_to_return = adata_dict[library_ids_ir[0]]
     if len(library_ids_ir) > 1:
-        adata = adata.concatenate([adata_dict[library_ids_ir[j]] for j in range(1,len(library_ids_ir))], batch_key="temp_batch")
+        adata_to_return = adata_to_return.concatenate([adata_dict[library_ids_ir[j]] for j in range(1,len(library_ids_ir))], batch_key="temp_batch")
 
     # validate that the assumption above holds, i.e. the batch corresponding to each corresponding_gex_lib is the same
     # for all cells in the adata_ir and is the same as the one in adata
     for j in range(len(library_ids_ir)):
         lib_id = library_ids_ir[j]
-        adata_ir_lib_id = adata[adata.obs["{}-library_id".format(lib_type)] == lib_id, :].copy()
+        adata_ir_lib_id = adata_to_return[adata_to_return.obs["{}-library_id".format(lib_type)] == lib_id, :].copy()
         ir_batch = adata_ir_lib_id.obs["temp_batch"]
         unique_ir_batch = np.unique(ir_batch)
         assert len(unique_ir_batch) == 1
@@ -257,8 +257,8 @@ def build_adata_from_ir_libs(lib_type: str, library_ids_ir: List[str]) -> AnnDat
 
     # we could keep this to know what batch (library) the cells are from, but we'll already have this via
     # the batch info in adata
-    del adata.obs["temp_batch"]
-    return adata
+    del adata_to_return.obs["temp_batch"]
+    return adata_to_return
 
 library_ids_bcr = []
 library_ids_tcr = []
