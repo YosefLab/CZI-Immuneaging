ceaae482ebcc2fae9bc1f87b278e08fbc9c0b156
Valeh Valiollah Pour Amiri <4193454+watiss@users.noreply.github.com>
2022-01-19 22:45:12 -0800

    more fix

--- data_processing/scripts/process_sample.py
+++ data_processing/scripts/process_sample.py
@@ -551,22 +551,24 @@ if not no_cells:
 
 # TODO remove this, used for now due to technical issues with reading adata back
 ct_T_cells = len(adata.obs[adata.obs["celltypist_predicted_labels.3"] == "T cells"])
-ir_T_cells = len(adata.obs[adata.obs["TCR-is_cell"] == "True"])
+ir_T_cells = len(adata.obs[(adata.obs["celltypist_predicted_labels.3"] == "T cells") & (adata.obs["TCR-is_cell"] == "True")])
 ir_ct_T_cell_pct = -1 if ct_T_cells==0 else (ir_T_cells/ct_T_cells) * 100
-logger.add_to_log("Out of {} T cells flagged by CellTypist, {} had TCR data (i.e. {:.2f}%)".format(ct_T_cells, ir_T_cells, ir_ct_T_cell_pct))
+logger.add_to_log("Out of {} cells labelled as T cells by CellTypist, {} had TCR data (i.e. {:.2f}%)".format(ct_T_cells, ir_T_cells, ir_ct_T_cell_pct))
 
 ct_B_cells = len(adata.obs[adata.obs["celltypist_predicted_labels.3"] == "B cells"])
-ir_B_cells = len(adata.obs[adata.obs["BCR-is_cell"] != "True"])
+ir_B_cells = len(adata.obs[(adata.obs["celltypist_predicted_labels.3"] == "B cells") & (adata.obs["BCR-is_cell"] == "True")])
 ir_ct_B_cell_pct = -1 if ct_B_cells==0 else (ir_B_cells/ct_B_cells) * 100
-logger.add_to_log("Out of {} B cells flagged by CellTypist, {} had BCR data (i.e. {:.2f}%)".format(ct_B_cells, ir_B_cells, ir_ct_B_cell_pct))
+logger.add_to_log("Out of {} cells labelled as B cells by CellTypist, {} had BCR data (i.e. {:.2f}%)".format(ct_B_cells, ir_B_cells, ir_ct_B_cell_pct))
 
 ct_non_T_cells = len(adata.obs[adata.obs["celltypist_predicted_labels.3"] != "T cells"])
-ir_ct_non_T_cell_pct = -1 if ct_non_T_cells==0 else (ir_T_cells/ct_non_T_cells) * 100
-logger.add_to_log("Out of {} non-T cells (per CellTypist), {} had TCR data (i.e. {:.2f}%)".format(ct_non_T_cells, ir_T_cells, ir_ct_non_T_cell_pct))
+ir_non_T_cells = len(adata.obs[(adata.obs["celltypist_predicted_labels.3"] != "T cells") & (adata.obs["TCR-is_cell"] == "True")])
+ir_ct_non_T_cell_pct = -1 if ct_non_T_cells==0 else (ir_non_T_cells/ct_non_T_cells) * 100
+logger.add_to_log("Out of {} cells not labelled as T cells by CellTypist, {} had TCR data (i.e. {:.2f}%)".format(ct_non_T_cells, ir_non_T_cells, ir_ct_non_T_cell_pct))
 
 ct_non_B_cells = len(adata.obs[adata.obs["celltypist_predicted_labels.3"] != "B cells"])
-ir_ct_non_B_cell_pct = -1 if ct_non_B_cells==0 else (ir_B_cells/ct_non_B_cells) * 100
-logger.add_to_log("Out of {} non-B cells (per CellTypist), {} had BCR data (i.e. {:.2f}%)".format(ct_non_B_cells, ir_B_cells, ir_ct_non_B_cell_pct))
+ir_non_B_cells = len(adata.obs[(adata.obs["celltypist_predicted_labels.3"] != "B cells") & (adata.obs["BCR-is_cell"] == "True")])
+ir_ct_non_B_cell_pct = -1 if ct_non_B_cells==0 else (ir_non_B_cells/ct_non_B_cells) * 100
+logger.add_to_log("Out of {} cells not labelled as B cells by CellTypist, {} had BCR data (i.e. {:.2f}%)".format(ct_non_B_cells, ir_non_B_cells, ir_ct_non_B_cell_pct))
 
 
 logger.add_to_log("Saving h5ad file...")
