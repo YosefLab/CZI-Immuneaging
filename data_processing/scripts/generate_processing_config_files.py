543bb95573efff1c1169a84088962cf33aa58f01
Valeh Valiollah Pour Amiri <4193454+watiss@users.noreply.github.com>
2022-02-05 09:50:40 -0800

    pass processed lib versions as cmd line params + comment out fastq deletion command in align_library

--- data_processing/scripts/generate_processing_config_files.py
+++ data_processing/scripts/generate_processing_config_files.py
@@ -1,7 +1,8 @@
 ## This script can be used to generate config files for all libraries and all samples from a given donor; currently only GEX libraries are considered (i.e. ignores BCR/TCR libs).
 ## Note that 
-## Run as follows: python generate_processing_config_files.py <config_type> <code_path> <output_destination> <donor_id> <seq_run> <s3_access_file>
+## Run as follows: python generate_processing_config_files.py <config_type> <code_path> <output_destination> <donor_id> <seq_run> <s3_access_file> <processed_gex_lib_version> <processed_ir_lib_version>
 ## where config_type can be one of "library", "sample" or "all"
+## processed_._lib_version args are only required if config_type is "sample", otherwise you can pass any string 
 ## Note that code_path, output_destination, and s3_access_file will be set later via generate_processing_scripts.py
 ## After generating the files run the following commands to upload to aws (after setting the aws credentials as env variables):
 ## aws s3 sync config_files s3://immuneaging/job_queue/process_library/ --exclude "*" --include "process_library*.configs.txt"
@@ -22,6 +23,8 @@ output_destination = sys.argv[3]
 donor_id = sys.argv[4]
 seq_run = sys.argv[5]
 s3_access_file = sys.argv[6]
+processed_gex_lib_version = sys.argv[7]
+processed_ir_lib_version = sys.argv[8]
 
 sys.path.append(code_path)
 from utils import *
@@ -125,7 +128,7 @@ if config_type in ["sample", "all"]:
             lib_ids = [i for i in samples[samples["Sample_ID"] == sample_id]["{} lib".format(lib_type)].iloc[0].split(",")]
             all_libs += lib_ids
             all_lib_types += [lib_type] * len(lib_ids)
-            processed_lib_version = "v2" if lib_type == "GEX" else "v1"
+            processed_lib_version = processed_gex_lib_version if lib_type == "GEX" else processed_ir_lib_version
             all_lib_versions += [processed_lib_version] * len(lib_ids)
 
         all_libs = []
