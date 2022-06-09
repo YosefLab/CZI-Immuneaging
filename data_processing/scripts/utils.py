16c6734f578a8ef47b2c96b61c6590084c9d8d6f
Valeh Valiollah Pour Amiri <4193454+watiss@users.noreply.github.com>
2022-06-08 20:11:33 -0700

    add fuzzy option to gdown download

--- data_processing/scripts/utils.py
+++ data_processing/scripts/utils.py
@@ -194,7 +194,7 @@ def read_immune_aging_sheet(sheet, output_fn=None, sheet_name=None, quiet=False)
 
     with warnings.catch_warnings(record=True) as w:
         warnings.filterwarnings("error")
-        output_fn = gdown.download(url, output_fn, quiet=quiet)
+        output_fn = gdown.download(url, output_fn, quiet=quiet, fuzzy=True)
 
         if len(w) == 1:
             msg = w[0]
