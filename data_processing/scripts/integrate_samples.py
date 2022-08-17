c0b6d8df52a1ff47317a6dc7ce08eec1b1f013a2
Valeh Valiollah Pour Amiri <4193454+watiss@users.noreply.github.com>
2022-08-17 15:35:29 -0700

    wip

--- data_processing/scripts/integrate_samples.py
+++ data_processing/scripts/integrate_samples.py
@@ -364,8 +364,10 @@ for integration_mode in integration_modes:
                     batch_vc = rna.obs["donor_id+tissue"].value_counts()
                     for b in batch_vc[batch_vc == 1].index.values:
                         barcode = rna[rna.obs[batch_key] == b].obs.index[0]
-                        logger.add_to_log(f"Removing cell {barcode} where {batch_key} = {b}.", level="warning")
+                        logger.add_to_log(f"Removing cell {barcode} where {batch_key} = {b} b/c it's the only one of its batch.", level="warning")
                         rna = rna[rna.obs[batch_key] != b,:].copy()
+                        # need to also remove it from adata
+                        adata = adata[adata.obs[batch_key] != b,:].copy()
                 else:
                     logger.add_to_log(f"Batch key {batch_key} not found in adata columns. Terminating execution.", level="error")
                     logging.shutdown()
