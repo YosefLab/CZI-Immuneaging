b3c52c998829ad15a7fd5db8bd1a5c2cc1c4f1b5
Valeh Valiollah Pour Amiri <4193454+watiss@users.noreply.github.com>
2022-12-20 10:20:45 -0800

    wip

--- data_processing/scripts/vdj_utils.py
+++ data_processing/scripts/vdj_utils.py
@@ -488,6 +488,7 @@ def report_vdj_lib_ss_and_fp_metrics_for_all_libs(
         t_adata_path: str,
         myeloid_adata_path: str,
         other_adata_path: str,
+        csv_file_path: str,
     ):
     assert ir_lib_type in ["BCR", "TCR"]
     cols = ['BCR-has_ir', 'TCR-has_ir', 'bcr_library_id', 'tcr_library_id']
@@ -495,14 +496,14 @@ def report_vdj_lib_ss_and_fp_metrics_for_all_libs(
     t_cells = anndata.read_h5ad(t_adata_path, backed=True).obs[cols]
     myeloid_cells = anndata.read_h5ad(myeloid_adata_path, backed=True).obs[cols]
     other_cells = anndata.read_h5ad(other_adata_path, backed=True).obs[cols]
-    
+
     def run_for_lib(
         lib_id: str,
         ir_lib_type: str,
         site: str,
         donor: str,
         organs: List[str],
-        skip_header: bool = False
+        skip_header: bool = False,
     ):
         cell_type = "b" if ir_lib_type == "BCR" else "t"
         print("Running for {} of type {}...".format(lib_id, ir_lib_type))
@@ -522,6 +523,7 @@ def report_vdj_lib_ss_and_fp_metrics_for_all_libs(
         receptors = cells[cells[has_ir_str] == "True"]
         num_receptors = len(receptors)
         # num_cells can be 0 e.g. in the case of SKIN
+        # it could also be that the corresponding GEX library failed
         pct_receptors = -1 if num_cells == 0 else (num_receptors * 100) / num_cells
         # look for any cell receptors of the "opposite" cell type
         opposite_ir_lib_type = "TCR" if ir_lib_type == "BCR" else "BCR"
@@ -537,7 +539,8 @@ def report_vdj_lib_ss_and_fp_metrics_for_all_libs(
         num_receptors_other_types = len(opposite_type_cells[opposite_type_cells[has_ir_str] == "True"])
         num_receptors_other_types += len(m_cells_lib[m_cells_lib[has_ir_str] == "True"])
         num_receptors_other_types += len(o_cells_lib[o_cells_lib[has_ir_str] == "True"])
-        pct_receptors_other_types = (num_receptors_other_types * 100) / num_cells_other_types
+        # num_cells_other_types could be 0 if for instance the corresponding GEX library failed
+        pct_receptors_other_types = -1 if num_cells_other_types == 0 else (num_receptors_other_types * 100) / num_cells_other_types
 
         print("✅✅ Results:")
         csv_rows = [
@@ -559,14 +562,20 @@ def report_vdj_lib_ss_and_fp_metrics_for_all_libs(
                 f"(# non-{cell_type} cells with {ir_lib_type} * 100)/# non-{cell_type} cells (aka false positive)": pct_receptors_other_types,
             }
         ]
-        csv_file = io.StringIO()
+        # csv_file = io.StringIO()
+        # field_names = list(csv_rows[0].keys())
+        # writer = csv.DictWriter(csv_file, fieldnames=field_names)
+        # if not skip_header:
+        #     writer.writeheader()
+        # writer.writerows(csv_rows)
+        # print(csv_file.getvalue())
+        # csv_file.close()
         field_names = list(csv_rows[0].keys())
-        writer = csv.DictWriter(csv_file, fieldnames=field_names)
-        if not skip_header:
-            writer.writeheader()
-        writer.writerows(csv_rows)
-        print(csv_file.getvalue())
-        csv_file.close()
+        with open(csv_file_path, "a") as f:
+            writer = csv.DictWriter(f, fieldnames=field_names)
+            if not skip_header:
+                writer.writeheader()
+            writer.writerows(csv_rows)
 
     ir_libs = list(get_vdj_lib_to_gex_lib_mapping()[0 if ir_lib_type == "BCR" else 1].keys())
     samples = read_immune_aging_sheet("Samples")
@@ -585,6 +594,6 @@ def report_vdj_lib_ss_and_fp_metrics_for_all_libs(
             raise ValueError("More than one donor was found for lib id {} of type {}".format(lib, ir_lib_type))
         site, donor = set(sites).pop(), set(donors).pop()
         # run for lib
-        run_for_lib(lib, site, donor, organs, skip_header=not first)
+        run_for_lib(lib, ir_lib_type, site, donor, organs, skip_header=not first)
         if first:
             first = False
