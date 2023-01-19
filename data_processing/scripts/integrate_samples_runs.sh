d7146eaa2deb3a024fd545a8f9d77b18ea7d4a82
Valeh Valiollah Pour Amiri <4193454+watiss@users.noreply.github.com>
2023-01-19 00:24:22 -0800

    Delete integrate_samples_runs.sh

--- data_processing/scripts/integrate_samples_runs.sh
+++
@@ -1,3 +0,0 @@
-source activate immune_aging.py_env.v4
-python integrate_samples.py /Users/valehvpa/Desktop/IA_stuff_temp/testing/integrate_samples.BAL.configs.txt
-python integrate_samples.py /Users/valehvpa/Desktop/IA_stuff_temp/testing/integrate_samples.BLO.configs.txt
